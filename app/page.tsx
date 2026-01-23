7eace934614cfd30c285a08390505da28e0b96eb
Helene <helenerasoavelo@gmail.com>
2026-01-23 10:00:40 +0300

    update the boutton at the section hero

--- app/page.tsx
+++ app/page.tsx
@@ -117,26 +117,26 @@ export default function Home() {
       <section className="bg-white lg:grid lg:h-screen lg:place-content-center">
         <div className="mx-auto w-screen max-w-7xl px-4 py-16 sm:px-6 sm:py-24 md:grid md:grid-cols-2 md:items-center md:gap-4 lg:px-8 lg:py-32">
           <div className="max-w-prose text-left">
-            <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl">
+            <h1 className="text-3xl lg:text-5xl font-bold text-gray-900 sm:text-5xl">
               Bonjour, je suis Hélène, développeuse Front-End.
             </h1>
 
-            <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
+            <p className="mt-5 text-base text-pretty text-gray-700 sm:text-lg/relaxed">
               Je conçois et développe des sites web et applications modernes,
               responsives et performantes. Je suis passionnée par la création
               d’expériences utilisateur intuitives et efficaces.
             </p>
 
-            <div className="mt-4 flex gap-4 sm:mt-6">
+            <div className="mt-4 m-10 flex flex-col sm:flex-row gap-6 sm:mt-6">
               <Link
-                className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
+                className="inline-block text-center justify-items-center rounded-3xl border bg-primary px-5 py-3 font-bold text-white"
                 href="#"
               >
                 Contactez-moi
               </Link>
 
               <Link
-                className="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900"
+                className="inline-block text-center rounded-3xl border border-gray-900 px-5 py-3 font-bold text-gray-900 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900"
                 href="#"
               >
                 Voir mon projets
